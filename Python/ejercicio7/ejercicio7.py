bebc3a6a55de5b185cc83e2cab44d528e2a9e2d4
Jose Antono Sánchez <jasbeem@gmail.com>
2022-09-22 12:22:40 +0200

    ejercicio7 resuelto

---
+++ Python/ejercicio7/ejercicio7.py
@@ -0,0 +1,9 @@
+import operaciones
+
+a=20
+b=2
+
+print(a,"+",b,"= ",operaciones.suma(a,b))
+print(a,"-",b,"= ",operaciones.resta(a,b))
+print(a,"*",b,"= ",operaciones.multiplica(a,b))
+print(a,"/",b,"= ",operaciones.divide(a,b))
